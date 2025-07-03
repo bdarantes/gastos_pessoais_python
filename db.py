fbd49892adf6dc821d2745ca8c1281a4dbfcf112
BrazD <brazdanarantes@gmail.com>
2025-07-03 10:37:17 -0300

    criação do db.py

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+def conectar_banco():
+	conn = sqlite3.connect('gastos.db')
+	return conn
+	
+	
+def criar_tabela():
+	conn = conectar_banco()
+	c = conn.cursor()
+	c.execute(''' CREATE TABLE IF NOT EXISTS gastos (
+	descricao TEXT NOT NULL,
+	valor REAL NOT NULL)''')
+	conn.commit()
+	conn.close()
+	
+def adicionar_gasto_bd(descricao, valor):
+	conn = conectar_banco()
+	c = conn.cursor()
+	c = execute("INSERT INTO gastos (descricao, valor) VALUES (?,?)", (descricao, valor))
+	conn.commit()
+	conn.close()
+	
+def listar_gastos():
+	conn = conectar_banco()
+	c = conn.cursor()
+	c.execute("SELECT * FROM gastos")
+	gastos = c.fetchall()
+	conn.close()
+	for gasto in gastos:
+		print (gasto)
+	
+	
+	
+
+	
